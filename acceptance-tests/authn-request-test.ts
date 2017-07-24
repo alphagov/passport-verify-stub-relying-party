ac46f67063d2934f448ccd67dc7735afd0b43470
Tuomas Nylund <tunylund@gmail.com>
2017-07-24 15:31:04 +0100

    BAU: Fix linting errors
    Authors: @tunylund @richardtowers

--- acceptance-tests/authn-request-test.ts
+++ acceptance-tests/authn-request-test.ts
@@ -11,7 +11,7 @@ describe('Clicking "Start"', function () {
   const VERIFY_SERVICE_PROVIDER_HOST = process.env['VERIFY_SERVICE_PROVIDER_HOST'] || 'http://localhost:50400'
   const COMPLIANCE_TOOL_URL = process.env['COMPLIANCE_TOOL_URL'] || 'https://compliance-tool-reference.ida.digital.cabinet-office.gov.uk/SAML2/SSO'
   let server: Server
-  
+
   beforeEach(done => {
     server = createApp({ verifyServiceProviderHost: VERIFY_SERVICE_PROVIDER_HOST }).listen(0, done)
   })
@@ -30,7 +30,7 @@ describe('Clicking "Start"', function () {
       expectedPID: 'passport-verify-stub-relying-party-pid',
       matchingServiceEntityId: 'http://passport-verify-stub-relying-party/msa',
       matchingServiceSigningPrivateKey: 'MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCHsxejk7eW9NucMiEFWCy+gtw4HGf/8NotDKRo0zzWeq7VImb2B4bWdmUH12wV5fhD7UXPfSwCdOH5LDJ84O232oKsuB5oosxRA7zq7kUJTI6ZiNRYJMbFR3oYnhXgibNGzYAqYrum1A9fiF3HydvpEkgdi9ElJSUx3bIQIKJLK1KP3k/+4q4IPkmPWmgnZ81/tJzh9v8WqMJ5Dnb7txVFXcaMAHYRP/MG/ycyuZrYiWHAJoljG/nAjkXlaYdxLwvLXhNUCyHvtOvvOIEgNDYHGjT9UtvmDloc3Z8Gno5zYqvtqh4UEg60U2MOMx+vzVuhRTfdngWyfOwKwoxfQiMjAgMBAAECggEBAIEsURdMORoAscBo05gTzFv5k4nyOGmhKv5xJ1wFoMcF98E5Q0t19LvX7epd/SyHQiHfDnIp1CfufWFE2jaXopI99eBWT9QJDHceGMqFz7+/WYr7fi79qx0GIkqmyWp5ieefHR6756cx/ARTefoBxE2EMpO1kXxLdLGYOLUza2ojQ57bygNXLyRZgqoDIdUK1gFiWZJefpUfwOpWkNTk9NkAkvGHWt/T30zN9bmX52zFFP8SKnBOdka+lk8iFHrwpd/8v1ass51WhNAjDafnBE2wMYQQTtIuaEhmZ7f8juEOC1NizXHkwMq9uXx3cAdXbVjs+rGsgjAwfD9BVAMvi0ECgYEA5YaKtC+O4pGxFVhG8ZsabvscmeVVkcHS7jVHU7Lwj/2NEZE2B7w5pdEJNWjNXRXqGUNUTwrapEIewyFcLGz1tbbBWAnve28e55J7WwI36wCmXD8v+LpuPfl9ZJxXO2ZOxhvWMlvh8pAaDzConc/W5/dU3V3sQUH99pAb96+vs6ECgYEAl1oLvVTMcPgq8cOiwnvJa1pav/w3QZCB+h+tTib1f8cgTcIuWMA0z4uDu3fBPXU6JvC//EwQYE9cNzdtDldxhdfeccwZoVka7jAmdYuu14DVtAPPqet2HL4AyIDKICofewGtxVDLR13EEmJegtkqVIZOcPeYYAj16SxybXySIEMCgYEAhni4srBaSiuJUDQT/GEer363VwKhi2+/IIhebY8cmX+3Ml+dBBMmwxshBgWMq8i3Cm4D6vs09Z83XqMg2XZMzlVwGSBZCjwkIxAS7VLzZ99NmCX8+QBgrjaJXHSsNsTNygttBrwGOJJschHT+AFYqzagpcDtNZ5wKBBuEkL/8YECgYBF1Y97IYhfS7KM8ObFc9ZhCUS8NsTMJMBER59wYvt9pMRb/I/j9XOom8gBlOT91XwqgYUkBXi854E2HQXdyy0fQ5ZozXK6BuItKtxj+jqHRvPT5rpHvdQ2uNilqv8YTjdOS10BoSDaYgJZNThEia1FaN8CssuE7D2DBDYcHJFT/wKBgBwmn7fkoSzrDypaaZzdK+BMPc/YLnIVgF7DF2zRv/laLr5FChf0n1Sdi5LrdPjUT0YmnFCZODhj3UV9PU9AoKX1ePAaxLs5X8pMQfeqW9lWbYWW2cE4on7QlPcKtJluBGboKAyvdI69t2sS+FoK2OiPqcWZLd3zBETYmJ/Hh95A',
-      userAccountCreationAttributes: [],
+      userAccountCreationAttributes: []
     }
 
     const req = request({
@@ -47,7 +47,7 @@ describe('Clicking "Start"', function () {
 
     req.end(JSON.stringify(postData))
   })
-  
+
   it('Should send an AuthnRequest to the compliance tool', async () => {
     await browser.visit(`http://localhost:${server.address().port}`)
     await browser.clickLink('Start')
