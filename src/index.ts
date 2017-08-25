d31deea4fa7cab3194e277f7d2a62d01171118a9
Rachael Booth <rachael.booth@digital.cabinet-office.gov.uk>
2017-08-25 15:31:28 +0100

    TT-999 Updated to use version 0.8.0 of passport-verify
    This means no longer passing a logger to the library, which now uses the 'debug' package.
    
    Authors: @rachaelbooth

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { createApp } from './app'
 
-const options = {
-  verifyServiceProviderHost: process.env.VERIFY_SERVICE_PROVIDER_HOST || 'http://localhost:50400',
-  logger: console
-}
-const server = createApp(options).listen(process.env.PORT || 3200, function () {
+const verifyServiceProviderHost = process.env.VERIFY_SERVICE_PROVIDER_HOST || 'http://localhost:50400'
+
+const server = createApp(verifyServiceProviderHost).listen(process.env.PORT || 3200, function () {
   console.log(`Stub RP app listening on port ${server.address().port}!`)
 })
