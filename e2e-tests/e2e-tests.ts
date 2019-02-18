b89d55da5285be01e4099f81e3026c67dba3a24a
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2019-02-18 07:47:30 +0000

    edKsCW79: Add E2E tests
    - added a simple e2e test
    - it's using puppeteer as zombie fails to interpret Hub's JavaScript
    - added IDs to the attribute views to differentiate matching vs non-matching
    - updated zombiejs along with this

---
+++ e2e-tests/e2e-tests.ts
@@ -0,0 +1,49 @@
+const assert = require('assert')
+const puppeteer = require('puppeteer')
+
+let browser
+let page
+let host = `https://passport-verify-stub-relying-party-${process.env.E2E_TEST_ENVIRONMENT || 'integration'}.cloudapps.digital/`
+
+before(async () => {
+  browser = await puppeteer.launch()
+  page = await browser.newPage()
+})
+
+after(async () => {
+  await browser.close()
+})
+
+function delay (timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout)
+  })
+}
+
+describe('A full non-matching journey', () => {
+  it('should succeed', async () => {
+    await page.goto(host, { waitUntil: 'networkidle2' })
+    await page.click('a.button-start')
+    await page.waitForSelector('input#start_form_selection_false')
+    await page.click('input#start_form_selection_false')
+    await page.click('input#next-button')
+    await page.waitForSelector("button[value='Stub Idp Demo One']")
+    await page.click("button[value='Stub Idp Demo One']")
+    await page.waitForSelector('input#username')
+    await page.type('input#username', 'stub-idp-demo-one')
+    await page.type('input#password', 'bar')
+    await page.click('input#login')
+    await page.waitForSelector('input#agree')
+    await page.click('input#agree')
+    // Due to multiple redirects at this stage we need to delay
+    await delay(1500)
+    const heading = await page.$eval('h2#heading-non-matching', e => e.innerHTML)
+    assert(heading.includes('Attributes for user with pid:'))
+  }).timeout(10000)
+})
+
+describe('A full matching journey', () => {
+  it('should succeed', async () => {
+    // TODO: once we deploy the compoments in a matching mode
+  }).timeout(10000)
+})
